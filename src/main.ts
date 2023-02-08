cb19d932d5521af9b7f5e8ec7a6e2f7c155cc413
josephxia <15906475@qq.com>
2023-02-08 11:53:44 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { generateDocument } from './doc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe())
 
   // 创建文档
   generateDocument(app)
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,15 +1,19 @@
 import { ApiProperty } from "@nestjs/swagger";
 
+import { IsNotEmpty, Matches, Max, Min, Length, IsEmail } from 'class-validator';
+
 
 export class CreateUserDto {
-    @ApiProperty({ example: '12321312321' })
+    @ApiProperty({ example: '18321312321' })
+    @Matches(/^1\d{10}$/g, { message: '请输入手机号' })
     phoneNumber: string;
 
-    @ApiProperty({ example: '111111' })
+    @ApiProperty({ example: '11111' })
+    @IsNotEmpty()
+    @Length(6, 10)
     password: string;
 
     @ApiProperty({ example: 'aa@qq.com' })
+    @IsEmail()
     email: string;
-
-
 }
