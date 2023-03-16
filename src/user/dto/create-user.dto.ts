74d83b1f6f9e1aa09ae0225c88334966ce2f8d74
josephxia <15906475@qq.com>
2023-03-16 10:54:13 +0800

    update

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+
+    /**
+    * 手机号（系统唯一）
+    */
+    @ApiProperty({ example: '13611177421' })
+    readonly phoneNumber: string;
+
+    @ApiProperty({ example: '然叔' }) name: string;
+
+    @ApiProperty({ example: '123456' })
+    password: string;
+
+    @ApiProperty({ example: '15906475@qq.com' })
+    email: string;
+
+}
