aa063302125ac15d8a62ced2b246861baa6b4dd7
josephxia <15906475@qq.com>
2023-03-21 22:30:49 +0800

    update

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -4,16 +4,52 @@ import { IsNotEmpty, Matches, Max, Min, Length, IsEmail } from 'class-validator'
 
 
 export class CreateUserDto {
-    @ApiProperty({ example: '18321312321' })
-    @Matches(/^1\d{10}$/g, { message: '请输入手机号' })
-    phoneNumber: string;
+    /**
+        * 手机号（系统唯一）
+        */
+    @Matches(/^1\d{10}$/g, { message: '请输入正确手机号' })
+    @IsNotEmpty({ message: '请输入手机号' })
+    @ApiProperty({ example: '13611177421' })
+    readonly phoneNumber?: string;
 
-    @ApiProperty({ example: '111111' })
+    @ApiProperty({ example: '然叔' })
     @IsNotEmpty()
-    @Length(6, 10)
-    password: string;
+    name?: string;
 
-    @ApiProperty({ example: 'aa@qq.com' })
-    @IsEmail()
-    email: string;
+    @ApiProperty({ example: '123456' })
+    @IsNotEmpty()
+    password?: string;
+
+    salt?: string;
+
+    @ApiProperty({ example: '15906475@qq.com' })
+    @IsNotEmpty()
+    email?: string;
+
+    @ApiProperty({ example: 'cookieboty' })
+    @IsNotEmpty()
+    avatar?: string;
+
+    @ApiProperty({ example: 'frontend' })
+    @IsNotEmpty()
+    job?: string;
+
+    @ApiProperty({ example: '前端开发工程师' })
+    @IsNotEmpty()
+    jobName?: string;
+
+    @ApiProperty({ example: 'cookieboty' })
+    @IsNotEmpty()
+    organization?: string;
+
+    @ApiProperty({ example: 'beijing' })
+    @IsNotEmpty()
+    location?: string;
+
+    @ApiProperty({ example: 'cookieboty' })
+    @IsNotEmpty()
+    personalWebsite?: string;
+
+    @ApiProperty({ example: '637855e9e8c408970ef9f4de' })
+    role?;
 }
