e5f3224789d2e6a2f3e1ce23a9e2a48af326278e
josephxia <15906475@qq.com>
2023-03-30 13:46:01 +0800

    update

---
+++ src/shared/interceptors/remove-sensitive-info.interceptor.ts
@@ -0,0 +1,32 @@
+import { NestInterceptor, Injectable, ExecutionContext, CallHandler } from '@nestjs/common';
+import { map } from 'rxjs';
+
+@Injectable()
+export class RemoveSensitiveUserInfoInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler) {
+        // console.log('RemoveSensitiveUserInfoInterceptor ...')
+        // console.log(`${context.getClass().name} ${context.getHandler().name}`)
+        const request = context.switchToHttp().getRequest()
+        // console.log('request', request.body)
+        return next.handle().pipe(
+            map(res => {
+                // 全局消除
+                this.delValue(res, 'password')
+                this.delValue(res, 'salt')
+                console.log('res', res)
+                return res
+            })
+        )
+    }
+
+    delValue(data, targetKey) {
+        for (let key in data) {
+            if (key === targetKey) {
+                delete data[key]
+            } else if (typeof data[key] === 'object') {
+                this.delValue(data[key], targetKey)
+            }
+        }
+    }
+
+}
